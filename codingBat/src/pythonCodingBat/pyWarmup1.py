876a97d399ba4c3c6294144e2d7daaac8c7a7b9f
KyleMueller <s1054831@monmouth.edu>
2020-08-29 23:58:39 -0400

    test again?

--- codingBat/src/pythonCodingBat/pyWarmup1.py
+++ codingBat/src/pythonCodingBat/pyWarmup1.py
@@ -1,7 +1,8 @@
-def main():
-    print("python solutions for codingbat.com")
-    
 def sleep_in(weekday, vacation):
   if not weekday or vacation:
+    return True
+  return False
+def monkey_trouble(a_smile, b_smile):
+  if a_smile == b_smile:
     return True
   return False
